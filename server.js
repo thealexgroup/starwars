2b241f27c4b34a1c52aedc15f102167457ca34f0
Tim Alex <Tim.Alex@enghouse.com>
2019-08-20 15:33:50 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,103 @@
+// Dependencies required
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+//define a port
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+//this is an array of objects in name/value pairs:
+var characters = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+//serve the add.html page at that route
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Get all characters
+app.get("/all", function(req, res) {
+
+  res.json(characters);
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/:characters?", function(req, res) {
+  var chosen = req.params.characters;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < characters.length; i++) {
+      if (chosen === characters[i].routeName) {
+        return res.json(characters[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(characters);
+});
+
+//this says that if nothing returns above, redirect by default to the view.html page
+app.get('*',function (req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+    });
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
